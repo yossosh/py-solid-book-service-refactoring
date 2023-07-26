30d643effd4652663d3cf03351661ab4e774976a
danylott <d.tuytuyshkin@gmail.com>
2023-07-26 16:21:04 +0200

    V1 initial task instructions

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Python Boilerplate For GitHub Tasks
+# SOLID Book Service Refactoring
 
 **Please note:** read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md)
 before starting.
+
+
+### Task
+
+You have the [system for working with books](app/main.py). But the code written is not following
+the best-practices. Please, refactor the existing code to follow SOLID principles,
+but make sure, that the logic is untouched, and all tests are passing.
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,58 @@
-# TODO: add tests
-from app.main import hello_world
+from io import StringIO
+import sys
+
+import pytest
+
+from app.main import main, Book
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+def get_stdout(func, *args, **kwargs) -> str:
+    new_stdout = StringIO()
+    old_stdout = sys.stdout
+    sys.stdout = new_stdout
+
+    func(*args, **kwargs)
+
+    sys.stdout = old_stdout
+    return new_stdout.getvalue()
+
+
+@pytest.fixture()
+def book() -> Book:
+    return Book("Sample Book", "This is some sample content.")
+
+
+def test_display_console(book) -> None:
+    output = get_stdout(main, book, [("display", "console")])
+    assert "This is some sample content." in output
+
+
+def test_display_reverse(book) -> None:
+    output = get_stdout(main, book, [("display", "reverse")])
+    assert "tnetnoc elpmas emos si sihT" in output
+
+
+def test_print_console(book) -> None:
+    output = get_stdout(main, book, [("print", "console")])
+    assert book.title in output
+    assert "This is some sample content." in output
+
+
+def test_print_reverse(book) -> None:
+    output = get_stdout(main, book, [("print", "reverse")])
+    assert book.title in output
+    assert "tnetnoc elpmas emos si sihT" in output
+
+
+def test_serialize_json(book) -> None:
+    serialized_book = main(book, [("serialize", "json")])
+    assert (
+        serialized_book
+        == '{"title": "Sample Book", "content": "This is some sample content."}'
+    )
+
+
+def test_serialize_xml(book) -> None:
+    serialized_book = main(book, [("serialize", "xml")])
+    assert "<title>Sample Book</title>" in serialized_book
+    assert "<content>This is some sample content.</content>" in serialized_book
