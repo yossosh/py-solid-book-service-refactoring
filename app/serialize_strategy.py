deefc57bd80ea5f08f3323218b12d2d321115384
Sofiia <sonagm9@gmail.com>
2024-07-02 20:43:13 +0200

    Fixed flake8 problem

--- app/serialize_strategy.py
+++ app/serialize_strategy.py
@@ -1,5 +1,5 @@
 import json
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
 from abc import ABC, abstractmethod
 from app.book import Book
 
@@ -17,9 +17,9 @@ class JsonSerializer(SerializeStrategy):
 
 class XmlSerializer(SerializeStrategy):
     def serialize(self, book: Book) -> str:
-        root = ET.Element("book")
-        title = ET.SubElement(root, "title")
+        root = ElementTree.Element("book")
+        title = ElementTree.SubElement(root, "title")
         title.text = book.title
-        content = ET.SubElement(root, "content")
+        content = ElementTree.SubElement(root, "content")
         content.text = book.content
-        return ET.tostring(root, encoding="unicode")
+        return ElementTree.tostring(root, encoding="unicode")
